3e7a23670246460f60d5fb750f7bdbafaeacb243
Zuzana Dostalova <dostalovaz@gmail.com>
2020-06-20 19:13:39 +0200

    Change pixel help for vector help

--- Assets/Scripty/Objekty/Napovedascript.cs
+++ Assets/Scripty/Objekty/Napovedascript.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-public class Napovedascript : MonoBehaviour {
+public class Napovedascript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        transform.DOScale(2f, 1f).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
 }
